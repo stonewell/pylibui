9f3874b1baf532e5e301865b4da3f50b2b08f8c0
Jingnan Si <jingnan.si@gmail.com>
2017-04-06 23:00:18 -0700

    1. using PyLong_AsVoidPtr instead of PyInt_AsLong 2. update windows libname with prefix

--- setup.py
+++ setup.py
@@ -5,14 +5,17 @@ import os
 
 extra_link_args = []
 runtime_library_dirs = []
+lib_name_prefix=''
 if platform.system() == 'Darwin':
     extra_link_args.append('-Wl,-rpath,' + os.path.join(distutils.sysconfig_get_python_lib(), 'pylibui'))
 elif platform.system() == 'Linux':
     runtime_library_dirs.append('$ORIGIN')
+elif platform.system() == 'Windows':
+    lib_name_prefix='lib'
 
 libui_module = Extension('pylibui._libui',
                              ['pylibui/libui.i'],
-                             libraries=['ui'],
+                             libraries=[lib_name_prefix+'ui'],
                              runtime_library_dirs=runtime_library_dirs,
                              extra_link_args = extra_link_args
                              )
