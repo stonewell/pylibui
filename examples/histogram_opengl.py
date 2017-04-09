da47597ec98a7306bcee6d85c3641599bdd48751
stonewell <jingnan.si@gmail.com>
2017-04-08 20:10:42 -0700

    check opengl version before calling opengl functions

--- examples/histogram_opengl.py
+++ examples/histogram_opengl.py
@@ -7,6 +7,7 @@ from pylibui.core import App
 from pylibui.controls import *
 from pylibui import libui
 from OpenGL.GL import *
+from OpenGL.GL.shaders import compileShader, compileProgram
 
 histogram = None
 
@@ -86,19 +87,98 @@ def constructGraph(width, height, extend):
 
     return path
 
+vertex_data = [
+    0.0, 0.0,
+    0.5, 0.0,
+    0.5, 0.5,
+
+    0.0, 0.0,
+    0.0, 0.5,
+    -0.5, 0.5,
+
+    0.0, 0.0,
+    -0.5, 0.0,
+    -0.5, -0.5,
+
+    0.0, 0.0,
+    0.0, -0.5,
+    0.5, -0.5,
+]
+
+from ctypes import sizeof, c_float, c_void_p, c_uint
+
+def init_buffers():
+    #/* We only use one VAO, so we always keep it bound */
+    vao = glGenVertexArrays(1)
+    glBindVertexArray(vao)
+
+    #/* This is the buffer that holds the vertices */
+    buffer = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, buffer)
+    glBufferData(GL_ARRAY_BUFFER,
+                     len(vertex_data) * 4,
+                     (GLfloat*len(vertex_data))(*vertex_data),
+                        GL_STATIC_DRAW)
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+
+    return(vao, buffer)
+
+fragment_src = '''
+#version 150
+
+out vec4 outputColor;
+
+void main() {
+  outputColor = vec4(1.0f, 0.f, 0.f, 1.0f);
+}
+'''
+
+vertex_src = '''
+#version 150
+
+in vec4 position;
+
+void main() {
+  gl_Position = position;
+}
+'''
+
+def init_shaders():
+    v_s = compileShader(vertex_src, GL_VERTEX_SHADER)
+    f_s = compileShader(fragment_src, GL_FRAGMENT_SHADER)
+    program = compileProgram(
+        v_s,
+        f_s
+        )
+
+    return (program, 0)
+
+def isOpenGLCoreProfile():
+    version = str(glGetString(GL_VERSION), 'utf-8').split(' ')[0]
+
+    major = int(version.split('.')[0])
+
+    return major >= 3
+
 class MyArea(OpenGLArea):
     def __init__(self):
         super().__init__()
+        self._program = None
+        self._buffer = None
 
     def onDraw(self, params):
         glClearColor(0, 1, 0, 0)
         glClear(GL_COLOR_BUFFER_BIT)
-        self.drawObject2()
+
+        if (isOpenGLCoreProfile()):
+            self.drawObject2_CoreProfile()
+        else:
+            self.drawObject2_Legacy()
         glFlush()
 
-    def drawObject2(self):
+    def drawObject2_Legacy(self):
         glMatrixMode(GL_PROJECTION)
-        
+
         glLoadIdentity()
         glDisable(GL_DEPTH_TEST)
         glDisable(GL_CULL_FACE)
@@ -106,38 +186,35 @@ class MyArea(OpenGLArea):
         glEnable(GL_TEXTURE_2D)
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        
-        glClearColor(0.0, 0.0, 0.0, 0.0)
-        
+
         glColor3f(1.0, 0.85, 0.35)
-        #glTranslatef(-1.5,0.0,-6.0)
-        
+
         glBegin(GL_TRIANGLES)
         glVertex3f( 0.0,  1.0, 0.0)
         glVertex3f(-1.0, -1.0, 0.0)
         glVertex3f( 1.0, -1.0, 0.0)
         glEnd()
-        
-        #glTranslatef(3.0,0.0,0.0);
-        #glBegin(GL_QUADS)
-        #glVertex3f(-1.0,-1.0, 0.0)
-        #glVertex3f( 1.0,-1.0, 0.0)
-        #glVertex3f( 1.0, 1.0, 0.0)
-        #glVertex3f(-1.0, 1.0, 0.0)
-        #glEnd()
 
-        glFlush()
+    def drawObject2_CoreProfile(self):
+        if self._buffer is None:
+            vao, self._buffer = init_buffers()
 
-    def drawObject(self):
-        glColor3f(1.0, 0.85, 0.35)
-        glBegin(GL_TRIANGLES)
+        if self._program is None:
+            self._program, mvp_location = init_shaders()
 
-        glVertex3f(  0.0,  0.6, 0.0)
-        glVertex3f( -0.2, -0.3, 0.0)
-        glVertex3f(  0.2, -0.3 ,0.0)
+        glUseProgram(self._program)
 
-        glEnd()
-        
+        glBindBuffer(GL_ARRAY_BUFFER, self._buffer)
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, c_void_p(0));
+
+        #/* Draw the three vertices as a triangle */
+        glDrawArrays(GL_TRIANGLES, 0, 12)
+
+        #/* We finished using the buffers and program */
+        glDisableVertexAttribArray(0)
+        glUseProgram(0)
+
     def onMouseEvent(self, e):
         graphWidth, graphHeight = graphSize(e.AreaWidth, e.AreaHeight)
         xs, ys = pointLocations(graphWidth, graphHeight)
@@ -150,10 +227,10 @@ class MyArea(OpenGLArea):
 
         global currentPoint
         currentPoint = found
-        
+
         #TODO only redraw the relevant area
         self.redrawAll()
-        
+
 app = App()
 
 window = MyWindow('pylibui Histogram Example', 640, 480)
