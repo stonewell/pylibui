959b708260e107cddef50830efe9b0b65d5e3161
stonewell <jingnan.si@gmail.com>
2017-04-09 22:37:46 -0700

    implement core profile histogram

--- examples/histogram_opengl.py
+++ examples/histogram_opengl.py
@@ -8,6 +8,7 @@ from pylibui.controls import *
 from pylibui import libui
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileShader, compileProgram
+import numpy as np
 
 histogram = None
 
@@ -53,57 +54,25 @@ def inPoint(x, y, xtest, ytest):
                 (y >= ytest - pointRadius) and
                 (y <= ytest + pointRadius))
 
-def pointLocations(width, height):
-    xincr, yincr = float(width) / 9, float(height) / 100
-
+def pointLocations():
     i = 0
-    xs = []
-    ys = []
+    vertex_data = []
+    vertex_data.extend([0.0, 0.0])
+    
     for spinbox in datapoints:
-        n = 100 - spinbox.getValue()
+        n = spinbox.getValue()
 
-        xs.append(xincr * i)
-        ys.append(yincr * n)
+        vertex_data.append(float(i) / 9)
+        vertex_data.append(float(n) / 100)
+
+        vertex_data.append(float(i + 1) / 9)
+        vertex_data.append(0)
 
         i += 1
 
-    return xs, ys
+    vertex_data.extend([1.0, 0.0])
 
-def constructGraph(width, height, extend):
-    xs, ys = pointLocations(width, height)
-    path = libui.uiDrawNewPath(libui.uiDrawFillModeWinding)
-
-    libui.uiDrawPathNewFigure(path, xs[0], ys[0])
-
-    for i in range(1, 10):
-        libui.uiDrawPathLineTo(path, xs[i], ys[i])
-
-    if extend:
-        libui.uiDrawPathLineTo(path, width, height);
-        libui.uiDrawPathLineTo(path, 0, height);
-        libui.uiDrawPathCloseFigure(path);
-
-    libui.uiDrawPathEnd(path)
-
-    return path
-
-vertex_data = [
-    0.0, 0.0,
-    0.5, 0.0,
-    0.5, 0.5,
-
-    0.0, 0.0,
-    0.0, 0.5,
-    -0.5, 0.5,
-
-    0.0, 0.0,
-    -0.5, 0.0,
-    -0.5, -0.5,
-
-    0.0, 0.0,
-    0.0, -0.5,
-    0.5, -0.5,
-]
+    return vertex_data
 
 from ctypes import sizeof, c_float, c_void_p, c_uint
 
@@ -114,34 +83,75 @@ def init_buffers():
 
     #/* This is the buffer that holds the vertices */
     buffer = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, buffer)
-    glBufferData(GL_ARRAY_BUFFER,
-                     len(vertex_data) * 4,
-                     (GLfloat*len(vertex_data))(*vertex_data),
-                        GL_STATIC_DRAW)
-    glBindBuffer(GL_ARRAY_BUFFER, 0)
 
     return(vao, buffer)
 
+def set_buffer_data(buffer, vertex_data, color):
+    glBindBuffer(GL_ARRAY_BUFFER, buffer)
+    glBufferData(GL_ARRAY_BUFFER,
+                     len(vertex_data) * 4 + len(color) * 4,
+                     c_void_p(0),
+                     GL_STATIC_DRAW)
+    glBufferSubData(GL_ARRAY_BUFFER,
+                        0,
+                        len(vertex_data) * 4,
+                        (GLfloat * len(vertex_data))(*vertex_data))
+    glBufferSubData(GL_ARRAY_BUFFER,
+                        len(vertex_data) * 4,
+                        len(color) * 4,
+                        (GLfloat * len(color))(*color))
+    
 fragment_src = '''
 #version 150
 
+in vec4 colorFromVertexShader;
 out vec4 outputColor;
 
 void main() {
-  outputColor = vec4(1.0f, 0.f, 0.f, 1.0f);
+  outputColor = colorFromVertexShader;
 }
 '''
 
 vertex_src = '''
 #version 150
 
+uniform mat4 translate;
+uniform mat4 scale;
 in vec4 position;
+in vec4 inputColor;
+out vec4 colorFromVertexShader;
 
 void main() {
-  gl_Position = position;
+  gl_Position = translate * scale * position;
+  colorFromVertexShader = inputColor;
 }
 '''
+
+# Provide a terse way to get a uniform location from its name
+def U(name):
+    p = glGetIntegerv(GL_CURRENT_PROGRAM)
+    return glGetUniformLocation(p, name)
+
+def A(name):
+    p = glGetIntegerv(GL_CURRENT_PROGRAM)
+    return glGetAttribLocation(p, name)
+
+# Provide a terse way to create a f32 numpy 3-tuple
+def V3(x, y, z):
+    return np.array([x, y, z], 'f')
+
+def translation(direction):
+    M = np.identity(4)
+    M[:3, 3] = direction[:3]
+    return M
+
+def scale(factor):
+    m = np.identity(4)
+    m[0, 0] = factor[0]
+    m[1, 1] = factor[1]
+    m[2, 2] = factor[2]
+
+    return m
 
 def init_shaders():
     v_s = compileShader(vertex_src, GL_VERTEX_SHADER)
@@ -169,9 +179,13 @@ class MyArea(OpenGLArea):
     def onDraw(self, params):
         glClearColor(0, 1, 0, 0)
         glClear(GL_COLOR_BUFFER_BIT)
+        
+        glViewport(0, 0, int(params.AreaWidth), int(params.AreaHeight))
+        
+        vertex_data = pointLocations()
 
         if (isOpenGLCoreProfile()):
-            self.drawObject2_CoreProfile()
+            self.drawObject2_CoreProfile(vertex_data)
         else:
             self.drawObject2_Legacy()
         glFlush()
@@ -195,7 +209,7 @@ class MyArea(OpenGLArea):
         glVertex3f( 1.0, -1.0, 0.0)
         glEnd()
 
-    def drawObject2_CoreProfile(self):
+    def drawObject2_CoreProfile(self, vertex_data):
         if self._buffer is None:
             vao, self._buffer = init_buffers()
 
@@ -204,24 +218,37 @@ class MyArea(OpenGLArea):
 
         glUseProgram(self._program)
 
-        glBindBuffer(GL_ARRAY_BUFFER, self._buffer)
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, c_void_p(0));
+        t = translation([-1.0, -1.0, 0.0])
+        s = scale([2.0, 2.0, 1.0])
+        
+        glUniformMatrix4fv(U("translate"), 1, True, t)
+        glUniformMatrix4fv(U("scale"), 1, True, s)
+
+        graphR, graphG, graphB, graphA = colorButton.getColor();
+
+        color = [graphR, graphG, graphB, graphA] * int(len(vertex_data) / 2)
+        
+        set_buffer_data(self._buffer, vertex_data, color)
+        
+        glEnableVertexAttribArray(A('position'))
+        glVertexAttribPointer(A('position'), 2, GL_FLOAT, GL_FALSE, 0, c_void_p(0));
+        
+        glEnableVertexAttribArray(A('inputColor'))
+        glVertexAttribPointer(A('inputColor'), 4, GL_FLOAT, GL_FALSE, 0, c_void_p(len(vertex_data) * 4));
 
         #/* Draw the three vertices as a triangle */
-        glDrawArrays(GL_TRIANGLES, 0, 12)
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, int(len(vertex_data) / 2))
 
         #/* We finished using the buffers and program */
         glDisableVertexAttribArray(0)
         glUseProgram(0)
 
     def onMouseEvent(self, e):
-        graphWidth, graphHeight = graphSize(e.AreaWidth, e.AreaHeight)
-        xs, ys = pointLocations(graphWidth, graphHeight)
+        vertex_data = pointLocations()
 
         found = -1
-        for i in range(10):
-            if inPoint(e.X, e.Y, xs[i], ys[i]):
+        for i in range(0, len(vertex_data), 2):
+            if inPoint(e.X, e.Y, vertex_data[i] * 9, vertex_data[i + 1] * 100):
                 found = i
                 break
 
